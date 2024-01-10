89cf361781d5a81ff5141bd2d26bc8562f67fc89
Luan Nguyen <luan.nguyen@codeenginestudio.com>
2024-01-10 11:17:47 +0700

    refactor

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/student")
 public class StudentController {
 
-    @GetMapping("/student")
+    @GetMapping("")
     public String home(){
         return "student";
     }
--- src/main/java/com/example/demo/controller/TeacherController.java
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/teacher")
 public class TeacherController {
 
-    @GetMapping("/teacher")
+    @GetMapping("")
     public String home(){
         return "teacher";
     }
