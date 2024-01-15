e4cb35608eeb611a8a7182ab75f82a4c0dd0ee40
Luan Nguyen <luan.nguyen@codeenginestudio.com>
2024-01-15 15:51:46 +0700

    initialize data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` VALUES
+(1,'$2a$10$bdoDxfpeNTgKdeaEmye5NO.ryMjv7WZbSmCPgCa7DZMUVa6onH7lO','luan.nguyen');
+
+INSERT INTO `roles` VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_GV'), (3, 'ROLE_SV');
+
+INSERT INTO `users_roles` VALUES (1,1);
